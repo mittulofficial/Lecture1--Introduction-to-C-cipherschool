b430862f3d8a74182310e5e0dc6017044dd26e8e
MITTUL KUMAR <kmittul2@gmail.com>
2023-06-26 15:10:22 +0530

    Initial Commit

---
+++ Lecture1-CipherSchools./main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lecture1-CipherSchools.
+//
+//  Created by MITTUL KUMAR on 26/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
